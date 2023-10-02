9892d1bc562cdf409bb8d795e78c804cff924424
GayaniDirect <gayanidirect@outlook.com>
2023-10-02 10:41:06 +0530

    Add Fundtransfer powergrid function to the system

---
+++ app/Livewire/Backend/Transaction/FundTransfer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Transaction;
+
+use Livewire\Component;
+
+class FundTransfer extends Component
+{
+    public function render()
+    {
+        return view('livewire..backend.transaction.fund-transfer')->layout('layouts.app');
+    }
+}
---
+++ resources/views/livewire/backend/transaction/fund-transfer.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div>
+        <x-backend.content-header title="Fund Transfers" />
+
+        <x-card title="" class="relative">
+            <livewire:backend.transaction.data-tables.fund-transfer-table/>
+        </x-card>
+    </div>
+</div>
+
+<script>
+    // document.getElementById("table-search").placeholder = "Search by payer NIC / payer Mobile";
+</script>
